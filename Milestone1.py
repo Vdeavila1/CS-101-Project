877006a0afe8801fcca91cb6e6a249083b29a139
Vdeavila1 <92134355+Vdeavila1@users.noreply.github.com>
2021-10-07 23:14:02 -0500

    Update Milestone1.py
    v10

--- Milestone1.py
+++ Milestone1.py
@@ -35,18 +35,18 @@ def GC_content(dna_list):
     return ( contentList.index( maximum ), maximum )
   
 def rna2codon(rna):
-## This function should accept a string representing an RNA sequence, and return the corresponding amino acid string, as transcribed by this codon table. \n",
-## You do not need to transcribe the stop codon.\n",
+## This function should accept a string representing an RNA sequence, and return the corresponding amino acid string, as transcribed by this codon table. 
+## You do not need to transcribe the stop codon.
     genetic_code = {
         'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',        'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
         'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',        'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
-    
+
         'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',        'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
         'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',        'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
-                        
+
         'UAU': 'Y', 'UAC': 'Y', 'UAA': 'STOP', 'UAG': 'STOP',        'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
         'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',        'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
-    
+
         'UGU': 'C', 'UGC': 'C', 'UGA': 'STOP', 'UGG': 'W',        'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
         'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',        'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
     }
@@ -55,6 +55,10 @@ def rna2codon(rna):
         return genetic_code[rna]
     else: 
         return 'Invalid'
+    amino = ''
+    for i in range( 0, int( len( rna ) / 3 ) ):
+        amino = amino + rna2codon(rna[3 * i: 3 * i + 3])
+    return amino
    
   
   
