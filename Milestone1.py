32c9246c0f9402009dac4e4c09342b31f43f470a
Vdeavila1 <92134355+Vdeavila1@users.noreply.github.com>
2021-10-08 18:08:06 -0500

    Update Milestone1.py
    v13

--- Milestone1.py
+++ Milestone1.py
@@ -58,8 +58,8 @@ def rna2codon(rna):
     stop = False
     i = 0
     while not stop:
-        codonString = rna[ i: i + 3]
-        if genetic_code[ codonString ] == ' * ':
+        codon_string = rna[ i: i + 3]
+        if genetic_code[ codonString ] == ' STOP ':
             stop = True
             continue
         output += 3
