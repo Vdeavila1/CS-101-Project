f17f4e88d7250ecb9ea9e255e30e5416716078f3
Vdeavila1 <92134355+Vdeavila1@users.noreply.github.com>
2021-11-05 20:45:44 -0500

    Update Milestone2.py
    Completed v1

--- Milestone2.py
+++ Milestone2.py
@@ -59,3 +59,133 @@ def assemble_genome( dna_list ):
                         last.remove( index )
                         return_list.append( ( v2, index, last ) )
         return answer
+      
+def shared_motif(dna_list):
+    String=dna_list[0]
+    StringLength=len(String)
+    Substrings=[]
+    Result = ""
+    for i in range(StringLength):
+        for n in range(i+1,StringLength+1):
+            #different lengths of substrings
+            test=String[i:n]
+            IsIn=True
+            for m in range(1,len(dna_list)):
+                #looks through all the other strings in the list
+                if test not in dna_list[m]:
+                    IsIn=False
+                    break #break out of loop if substring is not in string
+            if IsIn==True:
+                Substrings.append(test)
+    for Sub in Substrings:
+        if len(Sub)>len(Result): #Finds longest substring
+            Result=Sub
+    return Result
+
+print(shared_motif(["GATTACA","TAGACCA","ATACA"]))
+print(shared_motif(["ATATACA", "ATACAGA", "GGTATACA"]))
+
+
+# In[59]:
+
+
+import math
+def perfect_match(rna):
+    rna = rna.upper()
+    Acount = rna.count('A')
+    Ucount = rna.count('U')
+    Gcount = rna.count('G')
+    Ccount = rna.count('C')
+    AUcount = 0
+    GCcount = 0
+    count = 0
+    if Acount == Ucount:
+        AUcount = math.factorial(Acount)
+    if Gcount == Ccount:
+        GCcount = math.factorial(Gcount)
+    count = GCcount*AUcount
+    return count
+testrna = "AAACCCGGGUUU"
+print(perfect_match(testrna))
+
+
+def reverse_complement(dna):
+    dnaComp = ''
+    dnaList = list( dna.upper() )
+    dnaList.reverse()
+    for letter in dnaList:
+        if letter == 'A':
+            dnaComp += 'T'
+        elif letter == 'C':
+            dnaComp += 'G'
+        elif letter == 'G':
+            dnaComp += 'C'
+        elif letter == 'T':
+            dnaComp += 'A'
+    return dnaComp
+
+
+
+def rev_palindrome(dna):
+    ans = []
+    
+    for x in range(len(dna)):           #iterate over each letter in the string (x = index)
+    
+        for i in range(4,12):   #Loop using slice range from 4 to 12 characters
+            if (x + i) > len(dna):   #skip if index range oversteps bounds
+                continue
+            
+            chunk = dna[ x : x + i]       #set chunk using each length 4 to 12, starting at current index
+            revComp = reverse_complement( chunk )    #find reverse compliment of current chunk
+
+            if revComp == chunk:        #if reverse compliment equals original chunk
+                ans.append((x,i))    #add to list the position and length
+
+    return ans
+  
+  import math
+
+def random_genome(dna, gc_content):
+    ans = []
+    
+    at_count = dna.count('A') + dna.count('T')
+    gc_count = dna.count('G') + dna.count('C')
+
+    for x in gc_content:
+        gc_frequency = x/2
+        at_frequency = (1-x)/2
+    
+        gc_result = ((gc_frequency)**gc_count)
+        at_result = ((at_frequency)**at_count)
+        
+        ans.append( math.log10(gc_result * at_result) )
+    
+    return ans
+
+
+def get_edges(dna_dict):
+    dna_strings = list(dna_dict.values())   # list of all DNA strings
+    prefixes = []
+    suffixes = []
+    ans = []
+    
+    rev_dict = { v:k for k,v in dna_dict.items() }  #reverses inputted dictionary so we can change strings to identifiers
+
+    
+    for e in dna_strings:               #makes a list of suffixes & prefixes
+        prefixes.append( e[0:3] )       #first 3 letters
+        suffixes.append( e[-1:-4:-1] )  #last 3 letters
+    
+    for i in range(len(dna_strings)):   #iterate through each dna string
+
+        suf = suffixes[i]                 #set suffix for current string
+        
+        for x in range(len(suffixes)):                  #Iterate for each suffix
+            if i == x:                                  #skip when the prefix and suffix come from the same string
+                continue
+            elif suf == prefixes[x]:                      #find suffixes that match prefixes
+                preID = rev_dict.get(dna_strings[i])    #convert string with suffix to identifier
+                suffID = rev_dict.get(dna_strings[x])   #convert string with prefix to identifier
+                ans.append((preID,suffID))        # add IDs as a tuple to final list
+    
+    return ans
